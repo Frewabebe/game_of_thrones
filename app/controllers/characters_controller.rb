5fcca566b767daeabf881a170d92ba02ee32038e
Frew A Negash <zulufanke@yahoo.com>
2017-08-04 16:43:56 -0400

    edited Game of Thrones

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -8,4 +8,19 @@ class CharactersController < ApplicationController
   def show
      @characters = Character.find(params[:id])
    end
+
+   def update
+    @characters = Character.find(params[:id])
+    @character.update(character_params)
+
+    redirect_to character_path(@character)
+  end
+
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+
+    redirect_to character_path
+  end
+end
 end
--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -6,4 +6,19 @@ class HousesController < ApplicationController
  def show
     @house = House.find(params[:id])
   end
+
+
+  def update
+   @house = House.find(params[:id])
+   @house.update(house_params)
+
+   redirect_to house_path(@house)
+ end
+
+ def destroy
+  @house = House.find(params[:id])
+  @house.destroy
+
+  redirect_to house_path
+end
 end
